772fa4e4d34a9fcfbf053a5c58b46d639b1604fb
ekaterina <evts2006@gmail.com>
2024-04-15 20:17:10 +0300

    point 4 done

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def render_pygame(field, scr, scale):
     for y in range(0, len(field)):
         for x in range(0, len(field[0])):
             if field[y][x] == 0:
-                pygame.draw.rect(scr, (255, 255, 255), (x * scale, y * scale, scale, scale))
+                pygame.draw.rect(scr, (255, 255, 255), (x*scale, y*scale, scale, scale))
             elif field[y][x] == 1:
-                pygame.draw.rect(scr, (0, 0, 255), (x * scale, y * scale, scale, scale))
+                pygame.draw.rect(scr, (0, 0, 255), (x*scale, y*scale, scale, scale))
             # рисуем обводку
             pygame.draw.rect(scr, (0, 0, 0), (x * scale, y * scale, scale, scale), 2)
 
@@ -31,11 +31,22 @@ def main():
         print('Слишком много живых клеток')
         life_count = int(input('Введите изначальное количество живых клеток: '))
     scl = int(input('Введите масштаб отрисовки: '))
-    if width*scl >=640 or height*scl >=640:
+    if width*scl >=640:
         print('Поле не поместится на экране. Выберите меньший масштаб')
         scl = int(input('Введите масштаб отрисовки: '))
-
-    gof = GameOfLife(width, height)
+    print('Выберите тип автомата: 0 - по умолчанию; 1 - умирает, когда 0 или больше 5; 2 - оживает от 2 до 4 включительно')
+    type = int(input())
+    #height = width
+    if type == 1:
+        gof = GameOfLifeHeirOne(width, height)
+    elif type == 2:
+        gof = GameOfLifeHeirTwo(width, height)
+    elif type == 0:
+        gof = GameOfLife(width, height)
+    else:
+        print('Неверный тип')
+        print('Выберите тип автомата: 0 - по умолчанию; 1 - умирает, когда 0 или больше 5; 2 - оживает от 2 до 4 включительно')
+        type = int(input())
     gof.initialize(life_count)
     pygame.init()
     screen = pygame.display.set_mode((width*scl, height*scl))
@@ -45,7 +56,7 @@ def main():
     while is_running:
         for event in pygame.event.get():
             # проверить закрытие окна
-            if event.type == pygame.QUIT:
+            if  event.type == pygame.QUIT:
                 is_running = False
         gof.run_transition_rule()
         screen.fill((0, 0, 0))
@@ -54,7 +65,6 @@ def main():
         # держим цикл на правильной скорости
         clock.tick(60)
         pygame.time.delay(200)
-
 
 
 if __name__ == '__main__':
